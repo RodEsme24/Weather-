6ededf6b21e5f510c9505eb50cf6c7a4239dd32a
RodEsme24 <60196598+RodEsme24@users.noreply.github.com>
2020-05-08 16:44:04 -0700

    Add files via upload

--- weather.js
+++ weather.js
@@ -1,27 +1,22 @@
 $(document).ready(() => {
+  //Preparing local storage
+  let history = JSON.parse(localStorage.getItem("city"))
+  if(history) {
+    apiCall(history[0])
+  } else {
+    var array =[];
+    localStorage.setItem("city", JSON.stringify(array))
+  }
+
+  getLocalStorage();
+
   $("#citySumbit").click(() => {
     let city = $("#city").val();
-
-    const queryURL =
-      "https://api.openweathermap.org/data/2.5/forecast/?q=" +
-      city +
-      "&units=imperial&&appid=fa3026ffedb0a89a5f016059a673f69f";
-    $.ajax({
-      url: queryURL,
-      method: "GET",
-    })
-    .then(response => {
-      console.log(response);
-      $("#cityName").text(`${response.city.name}, ${response.city.country}`)
-      let totalChart ="";
-      for(let i = 0; i < 5; i++) {
-        let num = i*8 + 4;
-        let chart = cityWeather(response, num);
-        totalChart += chart;
-      }
-      $("#weatherResult").html(totalChart);
+    apiCall(city)
     });
   });
+
+
   function cityWeather(response, i) {
     return (
       `
@@ -33,6 +28,45 @@ $(document).ready(() => {
       `
     );
   }
-});
-
-  
+  function saveToLocalStorage(local) {
+    let currentStorage = JSON.parse(localStorage.getItem("city"))
+    currentStorage.unshift(local)
+    let stringifiedStorage = JSON.stringify(currentStorage);
+    localStorage.setItem("city", stringifiedStorage);
+    let retrievedVal = JSON.parse(localStorage.getItem("city"));
+    console.log(retrievedVal);
+  }
+  function apiCall(city) {
+    const queryURL =
+    "https://api.openweathermap.org/data/2.5/forecast/?q=" +
+    city +
+    "&units=imperial&&appid=fa3026ffedb0a89a5f016059a673f69f";
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    })
+    .then(response => {
+      console.log(response);
+      saveToLocalStorage(city);
+      getLocalStorage();
+      $("#cityName").text(`${response.city.name}, ${response.city.country}`)
+      let totalChart ="";
+      for(let i = 0; i < 5; i++) {
+        let num = i*8 + 4;
+        let chart = cityWeather(response, num);
+        totalChart += chart;
+      }
+      $("#weatherResult").html(totalChart);
+    })
+    .catch(err => {
+      alert("fam that is just not a city. What are you doing. Maybe it is a city but I dont understand. Pls fix ty")
+    })
+  }
+  function getLocalStorage() {
+    let retrievedVal=JSON.parse(localStorage.getItem("city"));
+    let data = "";
+    retrievedVal.forEach(item => {
+      data += `<li>${item}</li>`;
+    })
+    $("#history").html(data)
+  }
